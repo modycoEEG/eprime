d4e97aa1a3e03432a49259ce768cfceee2c964d1
modycoEEG <49270896+modycoEEG@users.noreply.github.com>
2019-04-16 13:15:07 +0200

    Add files via upload

--- stimShuffle.py
+++ stimShuffle.py
@@ -1,7 +1,7 @@
 import os, csv, random
 
 #os.chdir('C:\\Users\\jdyea\\OneDrive\\MoDyCo\\SWOP')
-os.chdir('C:\\Users\\AdminS2CH\\Documents\\GitHub\\eprime')
+# os.chdir('C:\\Users\\S2CH\\Desktop\\Experiments\\eprime')
 #%%
 catLabels = [112,114,122,124,131,132,133,134,141,142,143,144,212,214,222,224,
 			 231,232,233,234,241,242,243,244]
