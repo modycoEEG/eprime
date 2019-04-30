ed78e63f745a8e398be82c0aac4a233a62bc52b3
Jeremy Yeaton <jdyeaton27@gmail.com>
2019-04-30 17:22:23 +0200

    update stimshuffle

--- stimShuffle.py
+++ stimShuffle.py
@@ -6,21 +6,19 @@ import csv, random
 #%%
 catLabels = [112,114,122,124,131,132,133,134,141,142,143,144,212,214,222,224,
 			 231,232,233,234,241,242,243,244]
-#subNum = int(input("Subject number: "))
-subNum = 999
+subNum = int(input("Subject number: "))
 
-nLists = 10
+nLists = 10 # Number of blocks for stimulus presentation
+maxRep = 3 # Maximum number of the same response allowed in a row
 
 # Assign list number to even/odd subjects
-if subNum % 2 == 1:
+listNum = 2 # Default
+if subNum % 2 == 1: # Change if participant number is odd
 	listNum = 1
-else:
-	listNum = 2
 # Assign response category to alternate every 2 participants
-if subNum % 4 < 2: 
+responses = ['j','f'] # Default
+if subNum % 4 < 2: # Change for 2 out of every 4 subjects
 	responses = ['f','j']
-else:
-	responses = ['j','f']
 # Open practice stim file
 f = open('SWOPstims\\practice.txt','r')
 g = csv.reader(f,delimiter = '\t')
@@ -57,14 +55,14 @@ def checkRepeats(block,inarow):
 			inarow += 1
 		else:
 			inarow = 1
-		if inarow > 3:
+		if inarow > maxRep:
 			break
 	return inarow
 	
 # Shuffle
 random.shuffle(allItems) # Shuffle all the sentences
 newStim = []
-rpt,carryOver = 4, 1
+rpt,carryOver = maxRep + 1, 1
 for block in range(20): # 24 categories x 20 blocks = 480 items
 	currBlock = []
 	# For each block, find the first sentence with the desired category and append to block
@@ -78,16 +76,16 @@ for block in range(20): # 24 categories x 20 blocks = 480 items
 		toPop.sort(reverse = True)
 		for j in toPop: # Remove items that have been put in block
 			allItems.pop(j)
-	while rpt > 3:
+	while rpt > maxRep:
 		random.shuffle(currBlock)
 		if block > 0 and newStim[-1][5] == currBlock[0][5]:
-			rpt = checkRepeats(currBlock,carryOver + 1)
+			rpt = checkRepeats(currBlock, carryOver + 1)
 		else:
-			rpt = checkRepeats(currBlock,1)
+			rpt = checkRepeats(currBlock, 1)
 	for sent in currBlock:
 		newStim.append(sent) # Add sentences to stimulus list
 	carryOver = rpt
-	rpt = 4
+	rpt = maxRep + 1
 f.close()
 
 totItems = len(newStim)
@@ -95,8 +93,7 @@ iPerList = int(totItems / nLists)
 
 for listNum in range(1, nLists + 1):
 	f = open(''.join(['SWOPstims\\stimTextFiles\\stimBlock',str(listNum),'.txt']),'w')
-	f.write('\t'.join(allItems[0]))
-	f.write('\n')
+	f.write(''.join(['\t'.join(allItems[0]),'\n'])) # Write header
 	start = (listNum-1)*(iPerList)
 	for sent in newStim[start : start + iPerList]:
 		f.write(''.join(['\t'.join(sent),'\n']))
@@ -109,12 +106,11 @@ for line in newStim:
 f.close()
 
 # Save instructions
-f = open('SWOPstims\\expInstr%s.txt'%responses[0],'r')
-g = csv.reader(f,delimiter = '\t')
+f1 = open('SWOPstims\\expInstr%s.txt'%responses[0],'r')
+g = csv.reader(f1,delimiter = '\t')
 f2 = open(''.join(['SWOPstims\\stimTextFiles\\expInstr',str(subNum),'.txt']),'w')
 
-for line in g:
-	f2.write('\t'.join(line))
-	f2.write('\n')
-f.close()
+for line in g: # Copy instructions into subject-specific file
+	f2.write(''.join(['\t'.join(line),'\n']))
+f1.close()
 f2.close()
